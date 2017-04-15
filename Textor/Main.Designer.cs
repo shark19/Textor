6179ff16977bf095cc86ca3ef0c5bee38d81fde9
romas <romashkin3355@gmail.com>
2017-04-15 14:28:59 +0300

    Добавьте файлы проекта.

---
+++ Textor/Main.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Textor
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.MainRTB = new System.Windows.Forms.RichTextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.Create = new System.Windows.Forms.ToolStripButton();
+            this.Open = new System.Windows.Forms.ToolStripButton();
+            this.Save = new System.Windows.Forms.ToolStripButton();
+            this.SaveAs = new System.Windows.Forms.ToolStripButton();
+            this.Work1 = new System.Windows.Forms.ToolStripButton();
+            this.WorkRTB = new System.Windows.Forms.RichTextBox();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.Copy = new System.Windows.Forms.ToolStripButton();
+            this.Cut = new System.Windows.Forms.ToolStripButton();
+            this.Paste = new System.Windows.Forms.ToolStripButton();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel3 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainRTB
+            // 
+            this.MainRTB.Location = new System.Drawing.Point(12, 67);
+            this.MainRTB.Name = "MainRTB";
+            this.MainRTB.Size = new System.Drawing.Size(433, 473);
+            this.MainRTB.TabIndex = 1;
+            this.MainRTB.Text = "";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Create,
+            this.Open,
+            this.Save,
+            this.SaveAs,
+            this.toolStripLabel3,
+            this.Work1,
+            this.toolStripLabel2,
+            this.Copy,
+            this.Cut,
+            this.Paste,
+            this.toolStripLabel1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(922, 31);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // Create
+            // 
+            this.Create.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Create.Image = ((System.Drawing.Image)(resources.GetObject("Create.Image")));
+            this.Create.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Create.Name = "Create";
+            this.Create.Size = new System.Drawing.Size(28, 28);
+            this.Create.Text = "Create";
+            this.Create.Click += new System.EventHandler(this.create_Click);
+            // 
+            // Open
+            // 
+            this.Open.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Open.Image = ((System.Drawing.Image)(resources.GetObject("Open.Image")));
+            this.Open.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(28, 28);
+            this.Open.Text = "Open";
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // Save
+            // 
+            this.Save.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Save.Image = ((System.Drawing.Image)(resources.GetObject("Save.Image")));
+            this.Save.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(28, 28);
+            this.Save.Text = "Save";
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // SaveAs
+            // 
+            this.SaveAs.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.SaveAs.Image = ((System.Drawing.Image)(resources.GetObject("SaveAs.Image")));
+            this.SaveAs.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.SaveAs.Name = "SaveAs";
+            this.SaveAs.Size = new System.Drawing.Size(28, 28);
+            this.SaveAs.Text = "Save as";
+            this.SaveAs.Click += new System.EventHandler(this.SaveAs_Click);
+            // 
+            // Work1
+            // 
+            this.Work1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Work1.Image = ((System.Drawing.Image)(resources.GetObject("Work1.Image")));
+            this.Work1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Work1.Name = "Work1";
+            this.Work1.Size = new System.Drawing.Size(28, 28);
+            this.Work1.Text = "Do smth";
+            this.Work1.Click += new System.EventHandler(this.Work1_Click);  
+            // 
+            // WorkRTB
+            // 
+            this.WorkRTB.Location = new System.Drawing.Point(473, 67);
+            this.WorkRTB.Name = "WorkRTB";
+            this.WorkRTB.Size = new System.Drawing.Size(437, 473);
+            this.WorkRTB.TabIndex = 3;
+            this.WorkRTB.Text = "";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(22, 28);
+            this.toolStripLabel1.Text = "  ";
+            // 
+            // Copy
+            // 
+            this.Copy.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Copy.Image = ((System.Drawing.Image)(resources.GetObject("Copy.Image")));
+            this.Copy.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Copy.Name = "Copy";
+            this.Copy.Size = new System.Drawing.Size(28, 28);
+            this.Copy.Text = "Copy";
+            this.Copy.Click += new System.EventHandler(this.Copy_Click);
+            // 
+            // Cut
+            // 
+            this.Cut.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Cut.Image = ((System.Drawing.Image)(resources.GetObject("Cut.Image")));
+            this.Cut.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Cut.Name = "Cut";
+            this.Cut.Size = new System.Drawing.Size(28, 28);
+            this.Cut.Text = "Cut";
+            this.Cut.Click += new System.EventHandler(this.Cut_Click);
+            // 
+            // Paste
+            // 
+            this.Paste.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Paste.Image = ((System.Drawing.Image)(resources.GetObject("Paste.Image")));
+            this.Paste.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Paste.Name = "Paste";
+            this.Paste.Size = new System.Drawing.Size(28, 28);
+            this.Paste.Text = "Paste";
+            this.Paste.Click += new System.EventHandler(this.Paste_Click);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(22, 28);
+            this.toolStripLabel2.Text = "  ";
+            // 
+            // toolStripLabel3
+            // 
+            this.toolStripLabel3.Name = "toolStripLabel3";
+            this.toolStripLabel3.Size = new System.Drawing.Size(22, 28);
+            this.toolStripLabel3.Text = "  ";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(922, 552);
+            this.Controls.Add(this.WorkRTB);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.MainRTB);
+            this.Name = "Main";
+            this.Text = "Textor";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox MainRTB;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton Create;
+        private System.Windows.Forms.ToolStripButton Open;
+        private System.Windows.Forms.ToolStripButton Save;
+        private System.Windows.Forms.ToolStripButton SaveAs;
+        private System.Windows.Forms.ToolStripButton Work1;
+        private System.Windows.Forms.RichTextBox WorkRTB;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripButton Copy;
+        private System.Windows.Forms.ToolStripButton Cut;
+        private System.Windows.Forms.ToolStripButton Paste;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel3;
+    }
+}
+
---
+++ Textor/Main.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Textor
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private string curFileName;
+        private string selectedText;
+
+        private void create_Click(object sender, EventArgs e)
+        {
+            curFileName = null;
+            MainRTB.Clear();
+            WorkRTB.Clear();
+            this.Text = "Textor";
+        }
+
+        private void Open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            fillDialogProps(openFileDialog);
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    curFileName = openFileDialog.FileName;
+                    MainRTB.Text = System.IO.File.ReadAllText(curFileName); 
+                    this.Text = this.Text + " " + curFileName; 
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            if (curFileName != null)
+            {
+                System.IO.File.WriteAllText(curFileName, MainRTB.Text);
+            }
+            else
+            {
+                SaveAs_Click(sender, e);
+            }
+        }
+
+        private void SaveAs_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            fillDialogProps(saveFileDialog);
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    curFileName = saveFileDialog.FileName;
+                    Save_Click(sender, e);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not write to file. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void Work1_Click(object sender, EventArgs e)
+        {
+            WorkRTB.Clear();
+            string[] arr = MainRTB.Text.Split('\n');
+            List<string> list = new List<string>();
+            int len = 0;
+            foreach (string str in arr) {
+                if (str.Length > len) {
+                    len = str.Length;
+                }
+            }
+            foreach (string str in arr)
+            {
+                if (str.Length == len) list.Add(str);
+            }
+            foreach (string str in list)
+            {
+                str.Trim();
+                List<string> l = new List<string>(str.Split(' '));
+                l.Sort();
+                foreach (string word in l)
+                {
+                    WorkRTB.AppendText(word + " ");
+                }
+                WorkRTB.Text.TrimEnd();
+                WorkRTB.AppendText("\n");
+            }
+        }
+
+        private void fillDialogProps(FileDialog dialog)
+        {
+            dialog.InitialDirectory = "c:\\";
+            dialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            dialog.FilterIndex = 2;
+            dialog.RestoreDirectory = true;
+        }
+
+        private void Copy_Click(object sender, EventArgs e)
+        {
+            selectedText = MainRTB.SelectedText;
+        }
+
+        private void Cut_Click(object sender, EventArgs e)
+        {
+            Copy_Click(sender, e);
+            MainRTB.SelectedText = "";
+        }
+
+        private void Paste_Click(object sender, EventArgs e)
+        {
+            MainRTB.SelectedText = selectedText;
+        }
+    }
+}
